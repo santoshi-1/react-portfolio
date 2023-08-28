a20989d0b3471968ff95284a11c32c145ffcf749
santoshi-1 <karuturisantoshi@gmail.com>
2023-08-28 11:44:45 -0400

    added nav bar styles

--- src/sections/navbar/index.jsx
+++ src/sections/navbar/index.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import logo from '../../assets/logo.jpg'
+import data from "./data";
+import './navbar.css'
+import {IoIosColorPalette} from 'react-icons/io'
 
 const NavBar = () => {
   return (
     <nav>
-      <h2>NavBar</h2>
+      <div className="container nav__container" >
+        <a href="index.html" className="nav__logo">
+          <img src={logo} alt="Logo"/>
+        </a>
+        <ul className="nav__menu">
+          {
+            data.map(item => <li key={item.id}><a href={item.link}>{item.title}</a></li>)
+          }
+        </ul>
+        <button id="theme__icon"><IoIosColorPalette/></button>
+      </div>
     </nav>
   );
 };
