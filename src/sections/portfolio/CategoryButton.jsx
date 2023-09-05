01db243f57ec0af758c28629e5c2f4181eae673f
santoshi-1 <karuturisantoshi@gmail.com>
2023-09-05 13:36:27 -0400

    Updates

--- src/sections/portfolio/CategoryButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CategoryButton = ({category, className, onChangeCategory}) => {
-  return (
-    <button className={className} onClick={() => onChangeCategory(category)}>{category}</button>
-  )
-}
-
-export default CategoryButton
